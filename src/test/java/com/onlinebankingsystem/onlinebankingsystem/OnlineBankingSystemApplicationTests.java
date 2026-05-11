c9c652536bfccddf640dc6e7d51fc8eb3ec15c26
richeyville <richeyville@outlook.com>
2026-05-11 20:35:05 +0100

    Update OnlineBankingSystemApplicationTests.java

--- src/test/java/com/onlinebankingsystem/onlinebankingsystem/OnlineBankingSystemApplicationTests.java
+++ src/test/java/com/onlinebankingsystem/onlinebankingsystem/OnlineBankingSystemApplicationTests.java
@@ -4,10 +4,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class OnlineBankingSystemApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {}
 }
